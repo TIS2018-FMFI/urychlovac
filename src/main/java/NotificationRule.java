3560c1a26b68750d8b252afa4171108f59607147
dany05@email.cz <dany05@email.cz>
2019-02-04 19:07:21 +0100

    Configuration class ready, Notification class removed
    Niekde bol zmeneny typ <int> na <long> kvoli TOML formatu. Trieda NotificationManager teraz obsahuje atribut notificationRules typu List<NotificationRule>.

--- src/main/java/NotificationRule.java
+++ src/main/java/NotificationRule.java
@@ -1,11 +1,34 @@
+
 public class NotificationRule {
+    private String text;
+    private long level;
     private String rule;
+
+    public NotificationRule(String text, long level, String rule) {
+        this.text = text;
+        this.level = level;
+        this.rule = rule;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public void setRule(String rule) {
+        this.rule = rule;
+    }
+
 
     public String getRule() {
         return rule;
     }
-
-    public void setRule(String rule) {
-        this.rule = rule;
+    public String getText() {
+        return text;
     }
+    public long getLevel() {
+        return level;
+    }
+
 }
