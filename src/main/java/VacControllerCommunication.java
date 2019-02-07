56673b9256362b15c6d4d897c393b6ef53118bba
danshaman05 <dany05@email.cz>
2019-02-07 18:02:48 +0100

    Template for VacControllerCommunication ready.

---
+++ src/main/java/VacControllerCommunication.java
@@ -0,0 +1,11 @@
+public class VacControllerCommunication extends Thread {
+
+
+    public VacControllerCommunication() {
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
