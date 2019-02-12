ba6b8d58714e1d4421f35691b22c5938fc25c149
zuzhal <30445966+zuzhal@users.noreply.github.com>
2019-02-12 17:29:43 +0100

    pridanie kniznice csvtojson (#42)
    Squash merge

--- angular/src/app/shared/period.enum.ts
+++ angular/src/app/shared/period.enum.ts
@@ -18,14 +18,17 @@ export enum Period {
     {
       name: '1 deň',
       value: Period.ONE_D,
+      beValue: '1_DAY'
     },
     {
       name: '2 dni',
       value: Period.TWO_D,
+      beValue: '2_DAY'
     },
     {
       name: '5 dní',
       value: Period.FIVE_D,
+      beValue: '5_DAY'
     },
   ];
 
@@ -33,18 +36,22 @@ export enum Period {
     {
       name: '1 hodina',
       value: Period.ONE_H,
+      beValue: '1_HOUR'
     },
     {
       name: '2 hodiny',
       value: Period.TWO_H,
+      beValue: '2_HOUR'
     },
     {
       name: '6 hodín',
       value: Period.SIX_H,
+      beValue: '6_HOUR'
     },
     {
       name: '12 hodín',
       value: Period.TWELVE_H,
+      beValue: '12_HOUR'
     },
   ];
 
@@ -52,17 +59,21 @@ export enum Period {
     {
       name: '2 minúty',
       value: Period.TWO_M,
+      beValue: '2_MIN'
     },
     {
       name: '5 minút',
       value: Period.FIVE_M,
+      beValue: '5_MIN'
     },
     {
       name: '15 minút',
       value: Period.FIFTEEN_M,
+      beValue: '15_MIN'
     },
     {
       name: '30 minút',
       value: Period.THIRTY_M,
+      beValue: '30_MIN'
     },
   ];
