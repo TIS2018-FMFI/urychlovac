c3c7fc77c1fb201148d8e15a27382e579738a496
zuzhal <30445966+zuzhal@users.noreply.github.com>
2019-02-13 14:45:40 +0100

    zmena grafu komponentu, pridanie kniznice na spracovanie stringu v tvare csv (#47)

--- angular/src/app/shared/labData.model.ts
+++ angular/src/app/shared/labData.model.ts
@@ -1,7 +1,12 @@
 export class LabData {
     constructor(
         public id: number,
+<<<<<<< HEAD
+        public date: any,
+        public value: number
+=======
         public value: number,
         public date: Date
+>>>>>>> master
     ){}
 }
